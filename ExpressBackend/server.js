f7ae5ee8276c4c769926e91527e87c6a5f67024f
mjahanseir <mjahanseir@gmail.com>
2022-03-24 11:26:03 -0600

    first setup of backend finished

--- ExpressBackend/server.js
+++ ExpressBackend/server.js
@@ -23,4 +23,47 @@ const musicSchema= new mongoose.schema({
 // CREATE MODEL OBJECT BASED ON THE ABOVE SCHEMA OBJECT
 const Album = mongoose.model("Album", musicSchema);
 ///////////     5-     Routes      ///////////////////////////////////////////////////
+// 5-1- ROUTES
+// 5-2- INDEX ROUTE
+backend.get(  '/', function(req,res){
+    res.redirect("/music");
+})
+// 5-3- SHOW ROUTES
+// 5-4- GET ALL
+backend.get(  '/music', function(req,res){
+    Album.find({} , function( err , albums ){
+        if(err) {  return res .status(400).json( { success : false , error : err } );}
+        if( !albums.length){ return res.status(404).json( { success : false , error : "No albums found" } );}
+        return res .status(200).json( { success : true , data : albums} ); });
+})
 
+// 5-5- GET ONE
+backend.get('/music/:id' , function(req, res){
+    Album.findById(req.params.id, function(err, album){
+        if(err){return res.status(400).json( { success : false, error:err } );}
+        if(!album){return res.status(404).json( { success : false , error: "Album not found." } ); }
+        return res.status(200).json( { success : true ,  data: album});})
+})
+// 5-6- CREATE ROUTE
+backend.post('/music', function(req,res){
+    const body = req.body;
+    if(!body){ return res.status(400).json(  { success: false , error: "You must specify album information"});}
+    const album = new Album (body);
+    if( !album){return res.status(400).json( { success : false , error : "Album creation failed" } );}
+
+    album
+        .save().then(()=>{return res.status(201)
+                .json( { success:true,id: album._id,message:"Alnum created."} )   })
+        .catch(error => {return res.status(400).json( {error, message:"Album not created"});  })
+});
+// 5-7- UPDATE ROUTE
+// 5-8- DELETE ROUTE
+
+
+
+
+//GET LIST ALL ROUTE
+backend.listen(3001, function(){
+    console.log("Server started successfully");
+
+})
