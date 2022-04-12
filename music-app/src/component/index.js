06ceacefe8283658b197a5d467de87008f4d5627
mjahanseir <mjahanseir@gmail.com>
2022-04-12 10:45:30 -0600

    wrapping up

--- music-app/src/component/index.js
+++ music-app/src/component/index.js
@@ -24,3 +24,21 @@ import Logo from "./Logo";
 import NavBar from "./NavBar";
 
 export {NavBar, Logo,Link}
+
+///////////////////////////////////////////////////////////////////////////////
+
+//  summarize:
+
+// we added the use of the styled-components package which allows us to
+// create HTML components and add css styling, including assigning
+// bootstrap classes to components...
+
+// we created our navbar as separate pieces...
+// logo then links then pulled them together to make the whole navbar
+
+// we also used react Link components and React Fragments
+
+// At this point we're ready to commit our work and also publish...
+// once done, we'll also marge this particular branch in and will be
+// ready for our next feature branch for our next steps...
+
