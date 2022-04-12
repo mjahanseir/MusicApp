8c63271ea018f488d7291471029a1b5b3a4fc5bb
mjahanseir <mjahanseir@gmail.com>
2022-04-12 09:35:24 -0600

    links

---
+++ music-app/src/component/Link.jsx
@@ -0,0 +1,31 @@
+////////////////////////////////         step 2
+//import the same stuff from react and styled-components as in the last file plus...
+// we'll also import {Link} from react-router-dom
+
+// This last one is what we'll use to create our links to our List and Create pages
+
+//We're going to create three styled components here, all divs so we use
+// styled.div.attrs and then we'll pass a class to each with no style stuff
+// at the end of each...  we did that here but it must be in the
+//   main Album___ pages...
+
+//The three styled components we'll create will be called Collapse,
+// List and Item
+
+
+import React, {Component} from 'react';
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+
+class Links extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+
+export default Links;
