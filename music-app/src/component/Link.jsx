9ba1685beb59eb628eba6c6f752bc048cfefd101
mjahanseir <mjahanseir@gmail.com>
2022-04-12 10:03:53 -0600

    set up  Link.jsx

--- music-app/src/component/Link.jsx
+++ music-app/src/component/Link.jsx
@@ -16,20 +16,40 @@
 import React, {Component} from 'react';
 import styled from "styled-components";
 import {Link} from "react-router-dom";
+
+// add a className to each with the following class assignments in the same
+// syntax as before:
+// Collapse   className:'collapse navbar-collapse'
+// List       className:'navbar-nav mr-auto'
+// Item        className:'collapse navbar-collapse'
+
+// We're referencing bootstrap classes in our code here...
+// thats another little perk we get with using styled-components
+// Now that we have those HTML components declared,
+// we can create out main component
+// Same start as before.. we'll call this component Links
+// Once we have this set up, we'll start with a new kind of React thing
+// called a fragment
+// React allows for grouping together child elements into a fragment for
+// embedding in other components... that's what we're going to do here
+// We'll start by adding <React.Fragment> and its closing tag to our return (   )
 const Collapse= styled.div.attrs({
+    className:'collapse navbar-collapse'
 
 })``
 const List= styled.div.attrs({
-
+    className:'navbar-nav mr-auto'
 })``
 const Item = styled.div.attrs({
-
+    className: 'collapse navbar-collapse'
 })``
 class Links extends Component {
     render() {
-        return ({
+        return (
+        <React.Fragment>
 
-        });
+        </React.Fragment>
+    );
     }
 }
 
