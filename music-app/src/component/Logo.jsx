a3f8c3b4d9a86c2854502075a7aca9fdc9357e7e
mjahanseir <mjahanseir@gmail.com>
2022-04-12 08:34:10 -0600

    setup wrapper

--- music-app/src/component/Logo.jsx
+++ music-app/src/component/Logo.jsx
@@ -8,3 +8,24 @@
 import React, {Component} from "react";
 import styled from "styled-components";
 import logo from "../logo.svg";
+
+// "styled-components" This is a package that allows us to use basically CSS to
+// create react components within our files
+// use it to create basically like HTML pieces as components within our app
+//
+// first we're going to declare a const variable called Wrapper and assign it
+// code from styled.a.attrs so start with that
+
+// styled.a.attrs is actually method that will take an object as a parameter
+// which is a list of css class assignments
+
+//in this case, create an <a> component and now want to set a
+// bootstrap class to it
+// the <a> component is Wrapper
+// the class, to set is 'navbar-brand' so inside () for styled.a.attrs
+// is {} which then has a className: 'navbar-brand'
+const Wrapper= styled.a.attrs({
+    className: 'navbar-brand'
+})``  //////--->>>>  after the }) are two back ticks
+//In this case its empty but this part of the syntax allows us to also
+// set separate style on the component as well after setting a class
