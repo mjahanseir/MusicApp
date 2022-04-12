f3c4d9bef9856e5211122f87054271f0d9868a71
mjahanseir <mjahanseir@gmail.com>
2022-04-12 08:23:44 -0600

    import in logo.jsx

---
+++ music-app/src/component/Logo.jsx
@@ -0,0 +1,10 @@
+////////////////////////////////         step 1
+// we'll import both React and {Component} from react and
+// then import styled from the styled-components package on a second line.
+// These are just imports, not creating variables with require
+// then import the svg file as '../logo.svg'
+
+
+import React, {Component} from "react";
+import styled from "styled-components";
+import logo from "../logo.svg";
