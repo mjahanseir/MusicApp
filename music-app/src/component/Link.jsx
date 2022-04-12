11ea14b05b509e644e24685f554c2b02e57aa887
mjahanseir <mjahanseir@gmail.com>
2022-04-12 09:39:50 -0600

    set up constant for Link.jsx

--- music-app/src/component/Link.jsx
+++ music-app/src/component/Link.jsx
@@ -16,12 +16,20 @@
 import React, {Component} from 'react';
 import styled from "styled-components";
 import {Link} from "react-router-dom";
+const Collapse= styled.div.attrs({
 
+})``
+const List= styled.div.attrs({
+
+})``
+const Item = styled.div.attrs({
+
+})``
 class Links extends Component {
     render() {
-        return (
+        return ({
 
-        );
+        });
     }
 }
 
