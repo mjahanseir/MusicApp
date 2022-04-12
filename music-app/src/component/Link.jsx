1abadba1bc6ed72089f57b7277969d8d1705ed33
mjahanseir <mjahanseir@gmail.com>
2022-04-12 10:19:34 -0600

    Link.jsx is done

--- music-app/src/component/Link.jsx
+++ music-app/src/component/Link.jsx
@@ -33,6 +33,8 @@ import {Link} from "react-router-dom";
 // React allows for grouping together child elements into a fragment for
 // embedding in other components... that's what we're going to do here
 // We'll start by adding <React.Fragment> and its closing tag to our return (   )
+
+
 const Collapse= styled.div.attrs({
     className:'collapse navbar-collapse'
 
@@ -43,13 +45,61 @@ const List= styled.div.attrs({
 const Item = styled.div.attrs({
     className: 'collapse navbar-collapse'
 })``
+
+// inside this,
+// -->we'll add a Link component followed by
+// ----> a Collapse component...
+// -------> Inside Collapse, we'll add one List containing two Item components...
+
+// get our stuff together...
+// The Link component is exactly that...
+//      reacts version of an <a>...
+//      it takes two things: a target and some text to display.
+//      the target will be a path
+
+//notice how all of these components are added like regular html
+// we'll add the Link params similarly...
+//              to="/"     and       className="navbar-brand"
+// that goes inside the opening Link tag
+//          like <Link  to="" ... >
+// We'll add one of these to each of the Item tags as well for our links to
+// the list and create views
+// so for the first Item, we'll add a Link with
+//          to="" set to /music/list
+//          because that will be the route path to that view in our application
+//          (we'll be setting this up later)
+// The Link text for this first one will be List Albums
+// its className="" will be nav-link
+
+
+
+//Now add the second Item Link the same way with the same className,
+// the route path set to /music/create  and the text set to Create Album...
+
+// Note these routes aren't what is in our backend
+// these are routes we will setup within the frontend to point to particular
+// frontend views that we'll create
+
+// once you have the second link done, add the export line at the end,
+// exporting Links
+
 class Links extends Component {
     render() {
         return (
-        <React.Fragment>
-
-        </React.Fragment>
-    );
+            <React.Fragment>
+                <Link to="/" className="navbar-brand">CWEB 602 MERN Application</Link>
+                <Collapse>
+                    <List>
+                        <Item>
+                            <Link to="/music/list" className="nav-link">List Album</Link>
+                        </Item>
+                        <Item>
+                            <Link to="/music/create" className="nav-link">Create Album</Link>
+                        </Item>
+                    </List>
+                </Collapse>
+            </React.Fragment>
+        );
     }
 }
 
