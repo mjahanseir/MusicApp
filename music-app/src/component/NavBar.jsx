d1b2bea3616f286731bd7a2929dc29900685bba9
mjahanseir <mjahanseir@gmail.com>
2022-04-12 10:35:04 -0600

    navbar.jsx

--- music-app/src/component/NavBar.jsx
+++ music-app/src/component/NavBar.jsx
@@ -22,5 +22,26 @@ const Nav= styled.div.attrs({
     className:'navbar navbar-expand-lg navbar-dark bg-dark'
 })`margin-bottom: 20px;`
 
+///  the classname for container is just 'container'
+// for Nav, its 'navbar navbar-expand-lg navbar-dark bg-dark'
+// now we're going to add a set of `` to each.
+// these are for adding style properties and the syntax is just as
+// it would be in a css file...  property:  value;
+
+// for Container, add a height property with value 150px
+
+// for Nav, add a margin-bottom property with a value of 20px;
+
+// Once thats done, we'll create our NavBar component and add the above pieces...
+
+// this one will be really straightforward as there are no parameters,
+// just the components...
+
+// first a Container which will contain a Nav which will contain
+// the Logo and Links components
+
+// Note the last two are done as self terminating tags  <    />
+// rather than a pair like the others
+
 
 export default NavBar;
