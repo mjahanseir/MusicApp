4fc911d9ea498d01b5a23778a90eb951a2cc8711
mjahanseir <mjahanseir@gmail.com>
2022-04-14 11:25:00 -0600

    Album list setup set wrapper and styles

--- music-app/src/pages/AlbumList.jsx
+++ music-app/src/pages/AlbumList.jsx
@@ -3,3 +3,14 @@ import ReactTable from 'react-table'
 import styled  from 'styled-components'
 import 'react-table/react-table.css'
 import apis from'../api'
+
+/////////////////////////set wrapper and other styles
+const Wrapper =  styled.div`padding: 0 40px 0 40px;`
+const Update =  styled.div`
+    color:#0000ff;
+    cursor:pointer;
+`
+const Delete =  styled.div`
+    color:#ff0000;
+    cursor: pointer;
+`
