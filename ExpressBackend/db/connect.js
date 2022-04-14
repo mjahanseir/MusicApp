47d091893731f3285f5d4050af2d37b555b00012
mjahanseir <mjahanseir@gmail.com>
2022-04-14 16:27:21 -0600

    update connect

--- ExpressBackend/db/connect.js
+++ ExpressBackend/db/connect.js
@@ -1,17 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const DB ="mongodb://192.168.75.128:27017/music_app";
 
-///////////     2-    CREATE A CONNECTION TO THE MONGODB    ///////////////////////////
-const DB = "mongodb://192.168.75.128:27017/music_app";
-
-mongoose
-    .connect(DB)
-    .then(() => {
-        console.log("Batabase connection  successful.");
-    })
-    .catch((e) => {
-        console.error("connection error", e.message);
+mongoose.connect(DB)
+    .then(()=>{console.log("connected to db.")})
+    .catch((e)=>{
+        console.error("connection error",e.message);
     });
 
-const dbase = mongoose.connection;
+const dbase =mongoose.connection;
 
 module.exports = dbase;
