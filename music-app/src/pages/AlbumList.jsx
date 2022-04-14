23395133f6036ff3fc0444748adca865bc940d6e
mjahanseir <mjahanseir@gmail.com>
2022-04-14 11:28:46 -0600

    Album list setup UpdateAlbum

--- music-app/src/pages/AlbumList.jsx
+++ music-app/src/pages/AlbumList.jsx
@@ -15,23 +15,87 @@ const Delete =  styled.div`
     cursor: pointer;
 `
 /////////////////////////////////////////////////////////
+/////////////////// Create a components
+
+
+class UpdateAlbum extends Component{
+    updateUser= event =>{
+        event.preventDefault()
+        window.location.href=`music/update/${this.props.id}`
+    }
+    render(){
+        return <Update onClick={this.updateUser}>Update</Update>
+    }
+}
+
 
 
 
 
 class AlbumList extends Component{
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
+        this.state = {
             albums: [],
-            isLoading:false
+            isLoading: false
         }
     }
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-
-
     render(){
+        const {albums, isLoading} = this.state;
+
+        const columns = [
+            {
+                Header : 'Album Name',
+                accessor : 'album',
+                style: {'whiteSpace' : 'unset'},
+                Cell: row=> <div style={{textAlign:"center"}}>{row.value}</div>
+            },
+            {
+                Header : 'Artist Name',
+                accessor : 'artist',
+                style: {'whiteSpace' : 'unset'},
+                Cell: row=> <div style={{textAlign:"center"}}>{row.value}</div>
+            },
+            {
+                Header : 'Year Released',
+                accessor : 'year',
+                width:100,
+                Cell: row=> <div style={{textAlign:"center"}}>{row.value}</div>
+            },
+            {
+                Header : '',
+                accessor : '',
+                width:100,
+                Cell:function(props){
+                    return(
+                        <span>
+                                        <DeleteAlbum id={props.original._id} />
+                                    </span>
+                    )
+                }
+            },
+            {
+                Header : '',
+                accessor : '',
+                width:100,
+                Cell:function(props){
+                    return(
+                        <span>
+                                        <UpdateAlbum id={props.original._id} />
+                                    </span>
+                    )
+                }
+            },
+        ]
+
+
+
+        let showTable = true
+        if(!albums.length){
+            showTable= false;
+        }
+
 
         return(
             <Wrapper>
