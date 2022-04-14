eb79e78d9ee345355dfcd49f5c1705ea2c7eedb7
mjahanseir <mjahanseir@gmail.com>
2022-04-14 11:00:47 -0600

    NavBar.js is modiffied

--- music-app/src/component/NavBar.jsx
+++ music-app/src/component/NavBar.jsx
@@ -1,19 +1,21 @@
-////////////////////////////////         step 3
-//start with the same first two imports as the last two for react and styled
+/*///////////////////////////////         step 3
+        start with the same first two imports as the last two for react and styled
 
-// NavBar puts Logo and Links together so we need to import those into here...
-// add an import for each of these...
-// remember when you import something thats a default, you dont use the { }
+        NavBar puts Logo and Links together so we need to import those into here...
+        add an import for each of these...
+        remember when you import something thats a default, you dont use the { }
 
-// before we create our main component, we create a couple of styled ones...
+        before we create our main component, we create a couple of styled ones...
 
-// create styled div called Container and one called Nav as before...
-// set them both up ready to add class names and this time we'll use the ``at the end
+        create styled div called Container and one called Nav as before...
+        set them both up ready to add class names and this time we'll use the ``at the end */
 
 import React, {Component} from 'react';
 import styled from "styled-components";
 import Link from "./Links";
 import Logo from "./Logo";
+
+
 const Container= styled.div.attrs({
     className:'container'
 })`height: 150px;`
@@ -22,26 +24,27 @@ const Nav= styled.div.attrs({
     className:'navbar navbar-expand-lg navbar-dark bg-dark'
 })`margin-bottom: 20px;`
 
-///  the classname for container is just 'container'
-// for Nav, its 'navbar navbar-expand-lg navbar-dark bg-dark'
-// now we're going to add a set of `` to each.
-// these are for adding style properties and the syntax is just as
-// it would be in a css file...  property:  value;
+/*
+        the classname for container is just 'container'
+        for Nav, its 'navbar navbar-expand-lg navbar-dark bg-dark'
+        now we're going to add a set of `` to each.
+        these are for adding style properties and the syntax is just as
+        it would be in a css file...  property:  value;
 
-// for Container, add a height property with value 150px
+        for Container, add a height property with value 150px
 
-// for Nav, add a margin-bottom property with a value of 20px;
+        for Nav, add a margin-bottom property with a value of 20px;
 
-// Once thats done, we'll create our NavBar component and add the above pieces...
+        Once thats done, we'll create our NavBar component and add the above pieces...
 
-// this one will be really straightforward as there are no parameters,
-// just the components...
+        this one will be really straightforward as there are no parameters,
+        just the components...
 
-// first a Container which will contain a Nav which will contain
-// the Logo and Links components
+        first a Container which will contain a Nav which will contain
+        the Logo and Links components
 
-// Note the last two are done as self terminating tags  <    />
-// rather than a pair like the others
+        Note the last two are done as self terminating tags  <    />
+        rather than a pair like the others */
 
 class NavBar extends Component {
     render() {
