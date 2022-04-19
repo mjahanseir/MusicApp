fb0d41c0365b75df43e7b11d265796c0577c4cc8
Mohammad Jahanseir <48261288+mjahanseir@users.noreply.github.com>
2022-04-19 15:33:51 -0600

    setup index

--- Authentication/index.js
+++ Authentication/index.js
@@ -1,9 +1,30 @@
 //  npm i passport passport-local passport-local-mongoose express mongoose body-parser express-session
 const express = require("express"),
-    app = express(),
-    mongoose = require("mongoose"),
-    passport = require("passport"),
-    bodyParser = require("body-parser"),
-    localStrategy = require("passport-local"),
-    User = require("./model/user"),
-    expressSession = require("express-session");
+  app = express(),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  bodyParser = require("body-parser"),
+  localStrategy = require("passport-local"),
+  User = require("./model/user"),
+  expressSession = require("express-session");
+
+mongoose.connect("mongodb://192.168.75.128:27017/auth_demo");
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  expressSession({
+    secret: "sjhdklfhsdhfsdf24524s5fdsdfsdflklkfd85",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new localStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
