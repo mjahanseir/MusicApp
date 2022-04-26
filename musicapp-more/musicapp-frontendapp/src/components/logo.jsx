9250817493ccbdf8dccee450e8986c3ca6f638be
mjahanseir <mjahanseir@gmail.com>
2022-04-26 15:01:02 -0600

    Logo.jsx setup

---
+++ musicapp-more/musicapp-frontendapp/src/components/logo.jsx
@@ -0,0 +1,14 @@
+// react-router-dom
+// styled-components
+// bootstrap
+
+import React,{Component} from "react";
+import styled from 'styled-components';
+import logo from '../logo.svg';
+
+class Logo extends Component{
+    render() {
+        return();
+    }
+}
+export default logo;
