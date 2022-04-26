cfe79c14123648d6ed488e1e41d5310e3dc243bc
mjahanseir <mjahanseir@gmail.com>
2022-04-26 15:44:13 -0600

    List.jsx setup

---
+++ musicapp-more/musicapp-frontendapp/src/components/link.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import styled from 'styled-components';
+import{Link} from 'react-router-dom';
+
+const Collapse= styled.div.attrs({
+
+    })``
+
+const List= styled.div.attrs({
+
+    })``
+
+const Item= styled.div.attrs({
+
+    })``
