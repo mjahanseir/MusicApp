cf3644f41383a4c04cdbcabe414a8eb1725f567d
mjahanseir <mjahanseir@gmail.com>
2022-04-26 15:59:50 -0600

    List.jsx setup

--- musicapp-more/musicapp-frontendapp/src/components/link.jsx
+++ musicapp-more/musicapp-frontendapp/src/components/link.jsx
@@ -3,13 +3,22 @@ import styled from 'styled-components';
 import{Link} from 'react-router-dom';
 
 const Collapse= styled.div.attrs({
-
-    })``
+    className:'collapse navbar-collapse'
+})``
 
 const List= styled.div.attrs({
-
-    })``
+    className:'navbar-nav me-auto'
+})``
 
 const Item= styled.div.attrs({
+    className:'collapse navbar-collapse'
+})``
+class Link extends Component{
+    render() {
+        return(
+            <React.Fragment>
 
-    })``
+            </React.Fragment>
+        );
+    }
+}
