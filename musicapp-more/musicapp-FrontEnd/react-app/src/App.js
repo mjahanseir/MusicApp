abf157433e895cac273776955dbd9f67af9488ca
mjahanseir <mjahanseir@gmail.com>
2022-04-26 11:17:37 -0600

    react intro

--- musicapp-more/musicapp-FrontEnd/react-app/src/App.js
+++ musicapp-more/musicapp-FrontEnd/react-app/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state={
+            name:"Mike",
+            Age:18,
+            data:[
+                {
+                    "id":1,
+                    "name":"Dave",
+                    "age":"20"
+                },
+                {
+                    "id":2,
+                    "name":"Gilbert",
+                    "age":"30"},
+                {
+                    "id":3,
+                    "name":"Nathan",
+                    "age":"25"
+                }
+            ]
+        }
+    }
+    render() {
+        return (
+                 <table>
+                    <tbody>
+                    {this.state.data.map((person,i) => <TableRow key={i} dad={person} />)}
+
+                    </tbody>
+                </table>
+
+
+        );
+    }
 }
 
+class TableRow extends Component{
+    render(){
+        return(
+            <tr>
+                <td><h1> { this.props.dad.id }</h1> </td>
+                <td> { this.props.dad.name }</td>
+                <td> { this.props.dad.age }</td>
+
+            </tr>
+
+        )
+    }
+
+}
+App.defaultProps={
+    pName:"Jack",
+    pAge:"12"
+}
 export default App;
